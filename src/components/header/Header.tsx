1470137f273e3892b46d92805e3259c59c842ae2
theSun <sunjoojoo@163.com>
2022-03-08 10:58:34 +0800

    侧边栏与走马灯

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Header.module.css";
-import logo from "../../assets/logo.svg";
+import logo from "@/assets/logo.svg";
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 

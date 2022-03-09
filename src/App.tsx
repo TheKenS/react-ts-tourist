2c8dbc04799008df753680b4ead323402d82eaeb
theSun <sunjoojoo@163.com>
2022-03-09 20:41:37 +0800

    路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import styles from "./App.module.css";
-import { Header, Footer, SideMenu, Carousel } from "@/components";
-import { Row, Col } from "antd";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HomePage, SignInPage, RegisterPage, DetailPage } from "./pages";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <div className={styles["page-content"]}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/signIn" component={SignInPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/detail/:touristRouteId" component={DetailPage} />
+          <Route render={() => <h1>404 not found 页面去火星了 ！</h1>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

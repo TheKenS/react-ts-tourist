ee01f22dfc3c67cf2222ca6dad42cb0ed8d08580
theSun <sunjoojoo@163.com>
2022-03-17 16:07:11 +0800

    第八章

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
+import "./i18n/configs";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+import axios from "axios";
+
+axios.defaults.headers["x-icode"] = "32315AB42D24D4D3";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
